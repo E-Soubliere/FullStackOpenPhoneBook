3e3ca0a273d76f2b66a3517cc40a0ba01cb16b5d
Evan Soubliere <evan.soubliere@gmail.com>
2023-12-17 18:56:02 -0500

    person validation fix

--- models/person.js
+++ models/person.js
@@ -13,8 +13,21 @@ mongoose.connect(process.env.MONGODB_URI).then(result => {
 
 const personSchema = new mongoose.Schema({
     id: String,
-    name: String,
-    number: String,
+    name: {
+        type: String,
+        minLength: 3,
+        unique: true,
+        required: true
+    },
+    number: {
+        type: String,
+        validate: {
+            validator: function(v) {
+              return /\d{3,4}-\d{4}/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+    },
 })
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
